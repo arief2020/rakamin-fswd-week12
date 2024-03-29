8a67242869c35238961131b70be15d9364ea6647
arief2020 <msyaifullahalarief@gmail.com>
2024-03-29 20:05:57 +0700

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,137 @@
 import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from "../public/vite.svg";
 import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
-  const [count, setCount] = useState(0);
+const Square = ({ value, onSquareClick }) => {
+  return (
+    <button
+      className="border border-black w-24 h-24 p-0"
+      onClick={onSquareClick}
+    >
+      {value}
+    </button>
+  );
+};
 
+Square.propTypes = {
+  value: PropTypes.string,
+  onSquareClick: PropTypes.func,
+};
+
+const Board = ({ xIsNext, squares, onPlay }) => {
+  const handleClick = (i) => {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquare = squares.slice();
+    if (xIsNext) {
+      nextSquare[i] = 'X';
+    } else {
+      nextSquare[i] = 'O';
+    }
+    onPlay(nextSquare);
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner: ' + winner;
+  } else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="status">{status}</div>
+      <div className=" text-center ">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className=" text-center ">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className=" text-center ">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
     </>
   );
-}
+};
 
-export default App;
+Board.propTypes = {
+  xIsNext: PropTypes.bool,
+  squares: PropTypes.array,
+  onPlay: PropTypes.func,
+};
+export default function Game() {
+  const [currentMove, setCurrentMove] = useState(0);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
+  const handlePlay = (nextSquare) => {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquare];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  };
+
+  const jumpTo = (nextMove) => {
+    setCurrentMove(nextMove);
+  };
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to games start';
+    }
+    return (
+      <li
+        className="bg-slate-400 p-3 rounded-lg text-white mt-2 text-center"
+        key={move}
+      >
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+  return (
+    <div className="game flex justify-center">
+      <div>
+        <h1 className="text-3xl font-bold text-center">Tic Tac Toe</h1>
+        <div className="game-board max-w-xl mx-auto">
+          <Board
+            xIsNext={xIsNext}
+            squares={currentSquares}
+            onPlay={handlePlay}
+          />
+        </div>
+      </div>
+      <div className="game-info m-5 mt-10">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let index = 0; index < lines.length; index++) {
+    const [a, b, c] = lines[index];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
